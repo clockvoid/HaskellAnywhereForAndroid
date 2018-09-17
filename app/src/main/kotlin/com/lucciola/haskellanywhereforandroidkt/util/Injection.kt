d9567299091c9831cc5e83acb29d88564819cef0
Lucciola <hidenari.t1997nov@gmail.com>
2018-09-18 04:19:43 +0900

    add: injection

--- app/src/main/kotlin/com/lucciola/haskellanywhereforandroidkt/util/Injection.kt
+++ app/src/main/kotlin/com/lucciola/haskellanywhereforandroidkt/util/Injection.kt
@@ -1,2 +1,12 @@
 package com.lucciola.haskellanywhereforandroidkt.util
 
+import android.content.Context
+import com.lucciola.haskellanywhereforandroidkt.data.source.HaskellRepository
+import com.lucciola.haskellanywhereforandroidkt.data.source.remote.HaskellRemoteDataSource
+
+object Injection {
+
+    fun provideHaskellRepository(context: Context): HaskellRepository {
+        return HaskellRepository.getInstance(HaskellRemoteDataSource)
+    }
+}
