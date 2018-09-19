b48e4b704fea47d177f032d5eb854434cd7bedaf
Lucciola <hidenari.t1997nov@gmail.com>
2018-09-19 17:02:27 +0900

    fix: Haskell model

--- app/src/main/kotlin/com/lucciola/haskellanywhereforandroidkt/data/Haskell.kt
+++ app/src/main/kotlin/com/lucciola/haskellanywhereforandroidkt/data/Haskell.kt
@@ -5,12 +5,9 @@ import android.arch.persistence.room.Entity
 
 @Entity(tableName = "haskell")
 data class Haskell @JvmOverloads constructor (
-    @ColumnInfo(name = "Result") var Result: String = "Default",
-    @ColumnInfo(name = "Warnings") var Warnings: String = "",
-    @ColumnInfo(name = "Errors") var Errors: String = "",
-    @ColumnInfo(name = "Status") var Status: String = "",
-    @ColumnInfo(name = "Files") var Files: String = "",
-    @ColumnInfo(name = "AddedDefine") var AddedDefine: String = "",
-    @ColumnInfo(name = "Exception") var Exception: String = "",
-    @ColumnInfo(name = "HttpStatusCode") var HttpStatusCode: String = ""
+    @ColumnInfo(name = "Result") var Result: String? = "Default",
+    @ColumnInfo(name = "Warnings") var Warnings: String? = "",
+    @ColumnInfo(name = "Errors") var Errors: String? = "",
+    @ColumnInfo(name = "Status") var Status: String? = "",
+    @ColumnInfo(name = "Files") var Files: String? = ""
 )
