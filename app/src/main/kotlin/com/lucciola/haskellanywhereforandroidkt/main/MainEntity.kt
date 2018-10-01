2f479e7e365a4d1d7fc15cfe310aea6eba1e968e
Lucciola <hidenari.t1997nov@gmail.com>
2018-10-02 00:27:05 +0900

    add: entity Haskell

---
+++ app/src/main/kotlin/com/lucciola/haskellanywhereforandroidkt/main/MainEntity.kt
@@ -0,0 +1,11 @@
+package com.lucciola.haskellanywhereforandroidkt.main
+
+data class Haskell (
+        val mode: Int,
+        val message: String
+) {
+    companion object {
+        const val SUCCESS = 0
+        const val ERROR = 1
+    }
+}
