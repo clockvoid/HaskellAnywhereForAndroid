495288f2188b2d40613618f0a3f422efa89a16a0
Lucciola <hidenari.t1997nov@gmail.com>
2018-12-28 19:46:29 +0900

    delete: unrelated files

--- app/src/main/kotlin/com/lucciola/haskellanywhereforandroidkt/data/Program.kt
+++
@@ -1,8 +0,0 @@
-package com.lucciola.haskellanywhereforandroidkt.data
-
-data class Program (
-        val LanguageChoice: Int = 11,
-        val Program: String,
-        val Input: String,
-        val CompilerArgs: String = "-o a.out source_file.hs"
-)
--- app/src/main/kotlin/com/lucciola/haskellanywhereforandroidkt/interpreter/Task.kt
+++
@@ -1,6 +0,0 @@
-package com.lucciola.haskellanywhereforandroidkt.interpreter
-
-data class Task (
-    val process: (String) -> Unit,
-    val cancel: () -> Unit
-)
