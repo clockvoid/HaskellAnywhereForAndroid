8b05694738006f665a84dfb52ec69e47ba39384b
Lucciola <hidenari.t1997nov@gmail.com>
2019-01-05 22:27:06 +0900

    add: implementation of prelude fragment

--- app/src/main/kotlin/com/lucciola/haskellanywhereforandroidkt/prelude/PreludeFragment.kt
+++ app/src/main/kotlin/com/lucciola/haskellanywhereforandroidkt/prelude/PreludeFragment.kt
@@ -1,2 +1,29 @@
 package com.lucciola.haskellanywhereforandroidkt.prelude
 
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.lucciola.haskellanywhereforandroidkt.R
+import com.lucciola.haskellanywhereforandroidkt.databinding.PreludeFragmentBinding
+import com.lucciola.haskellanywhereforandroidkt.util.obtainViewModel
+
+class PreludeFragment : Fragment() {
+
+    private val viewModel: PreludeViewModel by lazy {
+        (activity as PreludeActivity).obtainViewModel(PreludeViewModel::class.java)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        val binding: PreludeFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.prelude_fragment, container, false)
+        val view = binding.root
+
+        binding.setLifecycleOwner(this@PreludeFragment)
+        binding.viewmodel = viewModel
+
+        return view
+    }
+}
