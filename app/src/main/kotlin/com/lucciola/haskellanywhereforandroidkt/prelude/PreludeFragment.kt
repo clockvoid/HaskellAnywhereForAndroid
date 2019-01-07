d70963d5fe3eb0d77809664ebab53ef7fc7c4ed2
Lucciola <hidenari.t1997nov@gmail.com>
2019-01-07 17:32:19 +0900

    apply: menu on prelude

--- app/src/main/kotlin/com/lucciola/haskellanywhereforandroidkt/prelude/PreludeFragment.kt
+++ app/src/main/kotlin/com/lucciola/haskellanywhereforandroidkt/prelude/PreludeFragment.kt
@@ -3,6 +3,7 @@ package com.lucciola.haskellanywhereforandroidkt.prelude
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.Toolbar
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -20,9 +21,13 @@ class PreludeFragment : Fragment() {
         super.onCreateView(inflater, container, savedInstanceState)
         val binding: PreludeFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.prelude_fragment, container, false)
         val view = binding.root
+        val toolbar: Toolbar = activity?.findViewById(R.id.toolbar)!!
 
         binding.setLifecycleOwner(this@PreludeFragment)
         binding.viewmodel = viewModel
+
+        toolbar.menu.clear()
+        toolbar.inflateMenu(R.menu.prelude_menu)
 
         return view
     }
